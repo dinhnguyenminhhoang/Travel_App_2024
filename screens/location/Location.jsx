bf666ecfd9c162616a158d63324aecb10f51ede5
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2024-01-08 17:34:05 +0700

    location page

--- screens/location/Location.jsx
+++ screens/location/Location.jsx
@@ -1,12 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import MapView, { Marker } from "react-native-maps";
 
 export default function Location() {
+    const coordinates = {
+        latitude: 35.6855,
+        longotude: 139.6884,
+        latitudeDelta: 0.01,
+        longotudeDelta: 0.01,
+        title: "my location",
+    };
     return (
-        <View>
-            <Text>Location</Text>
-        </View>
+        <MapView initialRegion={coordinates} style={styles.mapStyle}>
+            <Marker coordinate={coordinates} title={coordinates.title} />
+        </MapView>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    mapStyle: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
